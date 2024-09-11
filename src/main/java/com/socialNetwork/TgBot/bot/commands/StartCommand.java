8aa9f8d8118c377d905c7e2dd1b7f978c78244f2
GhostASA <artemsvist1809@gmail.com>
2024-09-11 13:21:19 +0300

    git init

---
+++ src/main/java/com/socialNetwork/TgBot/bot/commands/StartCommand.java
@@ -0,0 +1,38 @@
+package com.socialNetwork.TgBot.bot.commands;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Service
+@Slf4j
+public class StartCommand implements IBotCommand {
+
+
+    @Override
+    public String getCommandIdentifier() {
+        return "start";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Starts the TgBot";
+    }
+
+    @Override
+    public void processMessage(AbsSender absSender, Message message, String[] strings) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText("Test");
+
+        try {
+            absSender.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred in /start command", e);
+        }
+    }
+}
---
+++ src/test/java/com/socialNetwork/TgBot/TgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialNetwork.TgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
